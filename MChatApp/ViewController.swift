37d0463f156784a00772e7d75773bc7220ce6c3e
DedeepyaReddySalla <dedeepyareddy255@gmail.com>
2018-07-01 16:54:35 +0530

    Initial Commit

---
+++ MChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MChatApp
+//
+//  Created by Dedeepya reddy Salla on 7/1/18.
+//  Copyright © 2018 Motion Mentoring Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
