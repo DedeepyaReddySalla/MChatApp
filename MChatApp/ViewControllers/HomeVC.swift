7f9621c1b19e170b55c653438cb24b3145a2bfb1
DedeepyaReddySalla <dedeepyareddy255@gmail.com>
2018-07-01 19:31:10 +0530

    1.App Delegate - Added GIDSignInDelegate n its methods, created GIDSignIn instance 2.Added View controllers in storyboard n also created files for them

--- MChatApp/ViewControllers/HomeVC.swift
+++ MChatApp/ViewControllers/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  MChatApp
 //
 //  Created by Dedeepya reddy Salla on 7/1/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
